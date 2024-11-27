8a7caad505cf52b240e83335009a07d88b46d12c
Olivier Vignaud <ovi@ubiqube.com>
2024-11-27 16:30:39 +0100

    👽️ kafka: Pass tenantId in common dispatch.

--- event-kafka/src/main/java/com/ubiqube/etsi/mano/service/kafka/listener/KafkaCommonActionListener.java
+++ event-kafka/src/main/java/com/ubiqube/etsi/mano/service/kafka/listener/KafkaCommonActionListener.java
@@ -49,7 +49,7 @@ public class KafkaCommonActionListener {
 	@Transactional(TxType.NEVER)
 	public void onEvent(final ActionMessage ev) {
 		LOG.info("JMS Common ActionController Receiving Action: {}", ev);
-		actionController.dispatch(ev.getActionType(), ev.getObjectId(), ev.getParameters());
+		actionController.dispatch(ev.getActionType(), ev.getObjectId(), ev.getTenantId(), ev.getParameters());
 		LOG.info("JMS Common ActionController Done for event: {}", ev);
 	}
 
